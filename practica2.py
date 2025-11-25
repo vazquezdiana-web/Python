6ba68fcf49722b8b7b0c9137a841656b67bf2376
vazquezdiana-web <vazquezzdianaitzel@gmail.com>
2025-11-25 09:44:06 -0600

    actualizacion 25 de noviembre del 2025

---
+++ practica2.py
@@ -0,0 +1,20 @@
+#Actividad 2 (Aritmeticos).
+a = 10
+b = 3
+
+suma = a + b 
+resta = a - b 
+multiplicacion = a * b 
+division = a / b 
+división_entera = a // b 
+modulo = a % b 
+exponente = a ** b 
+
+#para imprimir
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(división_entera)
+print(modulo)
+print(exponente)
