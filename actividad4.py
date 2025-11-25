86c5cd5901c3cdd648a932a2f0aafffbdf36d663
vazquezdiana-web <vazquezzdianaitzel@gmail.com>
2025-11-25 09:03:41 -0600

    version del 25 de noviembre 2025

---
+++ actividad4.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+ventas = np.array([120, 95, 78, 110, 130, 125, 80, 60, 140, 150, 135,
+100])
+meses = ['Meses','Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre','Diciembre']
+df = pd.DataFrame({'Mes': meses, 'Ventas': ventas})
+print(df)
+print("Estadísticas de ventas:")
+print(df['Ventas'].describe())
+print("Mes con mayores ventas:", df.loc[df['Ventas'].idxmax()])
+print("Mes con menores ventas:", df.loc[df['Ventas'].idxmin()])
+media_ventas = df['Ventas'].mean()
+df['Clasificación'] = np.where(df['Ventas'] >= media_ventas, 'Alta','Baja')
+print("Clasificación de desempeño mensual:")
+print(df)
+resumen = df.groupby('Clasificación')['Ventas'].agg(['count', 'mean'])
+print("Resumen por nivel de desempeño:")
+print(resumen)
