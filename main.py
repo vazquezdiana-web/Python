4a666ffbf3d178f1254918e39be921471ca9f22d
vazquezdiana-web <vazquezzdianaitzel@gmail.com>
2025-12-01 10:35:44 -0600

    version 01 diciembre 2025

---
+++ main.py
@@ -0,0 +1,35 @@
+#Actividad 6: Crear un agente de IA
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+from agent import Agent
+
+load_dotenv()
+
+print("Mi primer agente de IA")
+
+client = OpenAI()
+agent = Agent()
+
+while True:
+    user_input = input("Tu: ").strip
+
+    if not user_input:
+        continue
+    if user_input.lower() in ("salir", "exit", "bye"):
+        print("Hasta luego!")
+        break
+
+    agent.messages.append({"role": "user", "content": user_input})
+    while True:
+        response =client.responses.create(
+            model="gtp-5-nano",
+            input=agent.messages,
+            tools=agent.tools
+        )
+
+        called_tool = agent.process_response(response)
+
+        if not called_tool:
+            break
